a7e24d5b6ddf3a071c66c3723b0db9422ca54d44
nicklytle <nal3gc@virginia.edu>
2018-05-31 16:35:52 -0400

    Setting up Grid of Scene

---
+++ DataStructureEdGame/Assets/Scenes/TestScence.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScence : MonoBehaviour {
+    public Transform ground;
+	// Use this for initialization
+	void Start () {
+        for(int j = 0; j < 16; j++)
+        {
+            for (int i = 0; i < 16; i++)
+            {
+                Instantiate(ground, new Vector2(i,j), Quaternion.identity);
+
+            }
+
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
