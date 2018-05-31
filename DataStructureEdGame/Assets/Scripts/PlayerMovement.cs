ea9b1869eda3813ee10354af4f1ace93d682e16b
nicklytle <nal3gc@virginia.edu>
2018-05-31 17:14:26 -0400

    Player Movement without gravity.

---
+++ DataStructureEdGame/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(new Vector2(0, 0.1f));
+
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(new Vector2(0, -0.1f));
+
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(new Vector2(-0.1f,0));
+
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(new Vector2(0.1f,0));
+
+        }
+    }
+}
--- DataStructureEdGame/Assets/Scripts/TestScence.cs
+++ DataStructureEdGame/Assets/Scripts/TestScence.cs
@@ -4,17 +4,20 @@ using UnityEngine;
 
 public class TestScence : MonoBehaviour {
     public Transform ground;
+    public Transform player;
 	// Use this for initialization
 	void Start () {
         for(int j = 0; j < 16; j++)
         {
-            for (int i = 0; i < 16; i++)
-            {
-                Instantiate(ground, new Vector2(i,j), Quaternion.identity);
+            //for (int i = 0; i < 16; i++)
+            //{
+                Instantiate(ground, new Vector2(j,0), Quaternion.identity);
 
-            }
+            //}
 
         }
+
+        Instantiate(player, new Vector2(3, 3), Quaternion.identity);
 
     }
 	
